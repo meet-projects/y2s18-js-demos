9ce7e2d977047668115d6d7f6a65d4335b46756e
Matthew Feng <mattfeng@mit.edu>
2018-08-01 01:37:38 +0300

    Content and style labs

---
+++ jquery-1/jquery-content/jquery-content.js
@@ -0,0 +1,5 @@
+// Your JavaScript and JQuery code goes here!
+
+// $("#main-idea").html("Fireballs are spicy!")
+// $(".content").eq(0).html("Fireballs are ROUND.")
+// $("p").eq(-2).html("Ted has lots of Fireballs.")
---
+++ jquery-1/jquery-events1/jquery-events1.js
@@ -0,0 +1,10 @@
+// Your JavaScript and JQuery code goes here!
+
+// var btn = document.getElementById("myBtn");
+// btn.addEventListener("click", function (event) {
+//   event.target.style.backgroundColor = "red";
+// });
+
+// $("#myBtn").on("click", function () {
+//   $(this).css("backgroundColor", "red");
+// });
