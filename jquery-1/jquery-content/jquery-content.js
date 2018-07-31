314eac06bc1ba901737fdefa4692d8086c9ce37c
Matthew Feng <mattfeng@mit.edu>
2018-08-01 02:10:29 +0300

    Update jquery content

--- jquery-1/jquery-content/jquery-content.js
+++ jquery-1/jquery-content/jquery-content.js
@@ -1,5 +1,5 @@
 // Your JavaScript and JQuery code goes here!
 
 // $("#main-idea").html("Fireballs are spicy!")
-// $(".content").eq(0).html("Fireballs are ROUND.")
+// $(".content").eq(0).html("<b>Fireballs are ROUND.</b>")
 // $("p").eq(-2).html("Ted has lots of Fireballs.")
